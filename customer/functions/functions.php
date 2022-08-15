dcfedef3400a910724c2200fc7a89ef2c1bcfa25
wolfflyy <ducanh11022001@gmail.com>
2022-08-15 10:02:28 +0700

    Update functions.php

--- customer/functions/functions.php
+++ customer/functions/functions.php
@@ -1,6 +1,6 @@
 <?php 
 
-$db = pg_connect("host=ec2-3-223-169-166.compute-1.amazonaws.com dbname=d94h92r35f62ln user=uxeervncpjcmmz password=d39e779b329374643f49f481395c02c2c32b64ef138a4c83e90f2f9fb922bc9b port=5432");
+$db = mysqli_connect("host=ec2-3-223-169-166.compute-1.amazonaws.com dbname=d94h92r35f62ln user=uxeervncpjcmmz password=d39e779b329374643f49f481395c02c2c32b64ef138a4c83e90f2f9fb922bc9b port=5432");
 
 /// begin getRealIpUser functions ///
 
