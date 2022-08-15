5f9e4ef2f3c5fec593ba49c6d61a4c90e5619e6f
wolfflyy <ducanh11022001@gmail.com>
2022-08-15 09:32:56 +0700

    Initial commit

---
+++ admin_area/includes/db.php
@@ -0,0 +1,5 @@
+<?php 
+
+$con = pg_connect("host=ec2-3-223-169-166.compute-1.amazonaws.com dbname=d94h92r35f62ln user=uxeervncpjcmmz password=d39e779b329374643f49f481395c02c2c32b64ef138a4c83e90f2f9fb922bc9b port=5432");
+
+?>
